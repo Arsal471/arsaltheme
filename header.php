eee6f9a181d75a0e61ff06f5b1cb28e4d0600419
Arsal471 <82088917+Arsal471@users.noreply.github.com>
2026-06-24 21:51:22 +0500

    Remove empty paragraph tag from header.php
    Removed empty paragraph tag from header.

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <p></p>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
