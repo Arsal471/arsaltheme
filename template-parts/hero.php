446269c0e1071bd8e8b6a2e3f9dce20fd08fe46e
arsal471 <arsalmustafa62@gmail.com>
2026-06-30 01:15:50 +0500

    third commit

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,17 +1,15 @@
-<div>
-<section class="hero" style="background-image: url('<?php echo get_theme_mod('arsal_hero_bg'); ?>');">
-    <div class="hero-content">
+<div class="hero-image hero-div" style="background-image: url('<?php echo get_theme_mod('arsal_hero_bg'); ?>');">
+<section class="hero-content">
         <h1><?php echo get_theme_mod('arsal_hero_title', 'Property pivot Buyers'); ?></h1>
         <p><?php echo get_theme_mod('arsal_hero_desc', 'Your Trusted Partner in Navigating Real Estate Challenges...'); ?></p>
-        <a href="#" class="btn"><?php echo get_theme_mod('arsal_hero_button', 'Read More'); ?></a>
-    </div>
+        <a href="#" class="hero-btn"><?php echo get_theme_mod('arsal_hero_button', 'Read More'); ?></a>
 </section>
      
-<form class="hero-form">
-    <input type="text" placeholder="<?php echo get_theme_mod('arsal_form_name_label', 'Enter your name'); ?>">
-    <input type="email" placeholder="<?php echo get_theme_mod('arsal_form_email_label', 'Enter your email'); ?>">
-    <input type="tel" placeholder="<?php echo get_theme_mod('arsal_form_phone_label', 'Enter your phone number'); ?>">
-    <input type="text" placeholder="<?php echo get_theme_mod('arsal_form_address_label', 'Your property address'); ?>">
+<form class="form">
+    <input type="text" placeholder="<?php echo get_theme_mod('arsal_form_name_label', 'Enter your name'); ?>"><br>
+    <input type="email" placeholder="<?php echo get_theme_mod('arsal_form_email_label', 'Enter your email'); ?>"><br>
+    <input type="tel" placeholder="<?php echo get_theme_mod('arsal_form_phone_label', 'Enter your phone number'); ?>"><br>
+    <input type="text" placeholder="<?php echo get_theme_mod('arsal_form_address_label', 'Your property address'); ?>"><br>
     <button type="submit"><?php echo get_theme_mod('arsal_form_button_text', 'Get an Offer'); ?></button>
 </form>
 </div>
